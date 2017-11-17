1e1a5d0bd0c942d044e8109985766e0ac24d87dd
Andrew Lloyd <andrew85.lloyd@gmail.com>
2017-11-17 02:39:06 -0800

    Validate the eventData for each event type

--- server/controllers/event.controller.js
+++ server/controllers/event.controller.js
@@ -1,7 +1,21 @@
 import Bluebird from 'bluebird';
 import EventStream from '../../api/EventStream';
+import Joi from 'joi';
+import paramValidation from '../../config/param-validation';
+import APIError from '../helpers/APIError';
 
 const producer = new EventStream.EventStreamProducer();
+
+function validateEventType(req, res, next) {
+	Joi.validate(paramValidation[req.body.eventType], req.body.eventData, (error) => {
+		if (error) {
+			var err = new APIError(error); //eslint-disable-line
+      next(error);
+		} else {
+			next();
+		}
+	})
+}
 
 function newEvent(req, res, next) {
 	const { eventType } = req.body;
@@ -9,9 +23,9 @@ function newEvent(req, res, next) {
 		if (err) {
 			res.json(err);
 		} else {
-			res.json(result);		
+			res.json(result);
 		}
 	});
 }
 
-export default { newEvent };
+export default { validateEventType, newEvent };
