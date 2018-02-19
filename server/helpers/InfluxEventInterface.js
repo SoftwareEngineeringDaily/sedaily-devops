59d927098929c1a1c6e5d7192c25efcff7c474d2
Andrew Lloyd <andrew85.lloyd@gmail.com>
2018-02-18 22:38:31 -0800

    Use API param validation for Influx event schema

--- server/helpers/InfluxEventInterface.js
+++ server/helpers/InfluxEventInterface.js
@@ -1,5 +1,24 @@
+import _ from 'lodash';
 import { FieldType, escape } from 'influx';
 import InfluxInterface from './InfluxInterface';
+import paramValidation from '../../config/param-validation';
+
+const fieldList = Object.keys(paramValidation.event.body);
+fieldList.splice(fieldList.indexOf('eventData'), 1);
+
+Object.keys(paramValidation).forEach((key) => {
+  const inner = paramValidation[key]._inner;
+  if (inner && inner.children) {
+    inner.children.forEach((child) => {
+      fieldList.push(child.key);
+    });
+  }
+});
+
+const fields = {};
+Array.from(new Set(fieldList)).forEach((field) => {
+  fields[field] = FieldType.STRING;
+});
 
 const databaseName = 'eventsDb';
 
@@ -8,21 +27,18 @@ export default class InfluxEventInterface extends InfluxInterface {
     super(databaseName, [
       {
         measurement: 'events',
-        fields: {
-          eventData: FieldType.STRING
-        },
-        tags: [
-          'eventType'
-        ]
+        fields,
+        tags: []
       }
     ]);
   }
 
   write(topic, eventData, callback) {
+    _.merge(eventData, eventData.eventData);
+    delete eventData.eventData; // eslint-disable-line no-param-reassign
     this.influx.writeMeasurement('events', [
       {
-        tags: { eventType: eventData.eventType },
-        fields: { eventData: JSON.stringify(eventData) }
+        fields: { ...eventData }
       }
     ])
       .then(() => {
