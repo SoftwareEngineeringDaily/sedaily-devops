f0c9000663a8d776d60b3b30c3aa968193538dd3
Andrew Lloyd <andrew85.lloyd@gmail.com>
2018-02-23 18:32:12 -0800

    add register event. remove extraneous describe statement

---
+++ tests/server/events/register.event.js
@@ -0,0 +1,27 @@
+import request from 'supertest-as-promised';
+import httpStatus from 'http-status';
+import chai, { expect } from 'chai';
+import app from '../../../index';
+
+chai.config.includeStack = true;
+
+describe('## Register Events', () => {
+  it('sending valid register event is successful', (done) => {
+    const event = {
+      clientId: '1234567',
+      deviceType: 'Android',
+      eventTime: new Date().getTime(),
+      eventType: 'register',
+      eventData: {}
+    };
+    request(app)
+      .post('/api/v1/event')
+      .send(event)
+      .expect(httpStatus.OK)
+      .then((res) => {
+        expect(res.body).to.exist; //eslint-disable-line
+        done();
+      })
+      .catch(done);
+  });
+});
