790f08cf965ddbd35a4ee58b0d8268dca64c1a8e
Andrew Lloyd <andrew85.lloyd@gmail.com>
2018-02-23 17:30:36 -0800

    Add event tests

--- tests/server/events/event.test.js
+++ tests/server/events/event.test.js
@@ -76,6 +76,46 @@ describe('## Basic Event APIs', () => {
         .catch(done);
     });
 
+    it('errors when no deviceType sent', (done) => {
+      const event = {
+        clientId: '45426562',
+        eventTime: new Date().getTime(),
+        eventType: 'login',
+        eventData: {
+          userId: '8675309'
+        }
+      }
+      request(app)
+        .post('/api/v1/event')
+        .send(event)
+        .expect(httpStatus.BAD_REQUEST)
+        .then((res) => {
+          expect(res.body).to.exist; //eslint-disable-line
+          expect(res.body.message).to.equal('"deviceType" is required'); //eslint-disable-line
+          done();
+        })
+        .catch(done);
+    });
+
+    xit('errors when no eventData sent', (done) => {
+      const event = {
+        clientId: '45426562',
+        deviceType: 'API',
+        eventTime: new Date().getTime(),
+        eventType: 'login'
+      }
+      request(app)
+        .post('/api/v1/event')
+        .send(event)
+        .expect(httpStatus.BAD_REQUEST)
+        .then((res) => {
+          expect(res.body).to.exist; //eslint-disable-line
+          expect(res.body.message).to.equal('"eventData" is required'); //eslint-disable-line
+          done();
+        })
+        .catch(done);
+    });
+
     it('errors when no eventType sent', (done) => {
       const event = {
         clientId: '1234567',
