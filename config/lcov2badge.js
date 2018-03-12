ec9dcadd640d6cf497be6d7e93254a7497b95529
Andrew Lloyd <andrew85.lloyd@gmail.com>
2018-03-11 20:54:53 -0700

    Use webtask API to update coverage badge

--- config/lcov2badge.js
+++ config/lcov2badge.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const request = require('request');
 const lcov2badge = require('lcov2badge');
 
 const filePath = process.argv[2];
@@ -9,9 +9,24 @@ const options = {
   koThreshold: 60, // default is 60
 };
 
+function sendCoverageBadge(coverageBadge) {
+  const requestOptions = {
+    method: 'post',
+    body: { coverageBadge, coverageBadgeToken: process.env.COVERAGE_BADGE_TOKEN },
+    json: true,
+    url: process.env.COVERAGE_BADGE_URL
+  };
+  request(requestOptions, (err, res, body) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(body.result); // eslint-disable-line no-console
+    }
+  });
+}
+
+
 lcov2badge.badge(options, (err, svgBadge) => {
   if (err) throw err;
-  fs.writeFile('coverageBadge.svg', svgBadge, (error) => {
-    if (err) throw error;
-  });
+  sendCoverageBadge(svgBadge);
 });
