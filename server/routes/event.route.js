cb51b8cb516559bc8a695b6587e8c992d8677b91
AbdulBasit Kabir <abdulbasitkay@users.noreply.github.com>
2018-04-09 21:32:34 +0100

    Add swagger doc (#70)
    * Add swagger doc

--- server/routes/event.route.js
+++ server/routes/event.route.js
@@ -8,6 +8,38 @@ const router = express.Router();
 // Disabling validation for now
 // router.use(expressJwt({ secret: config.jwtSecret }));
 
+/**
+ * @swagger
+ * tags:
+ * - name: event
+ *   description: Sending user events
+ */
+
+/**
+ * @swagger
+ * /event:
+ *   post:
+ *     summary: Stream an event
+ *     description: Stream an event
+ *     tags: [event]
+ *     parameters:
+ *       - in: body
+ *         name: event
+ *         schema:
+ *             $ref: '#/definitions/Event'
+ *         required: true
+ *         description: Event to stream
+ *     responses:
+ *       '200':
+ *         $ref: '#/responses/Success'
+ *       '400':
+ *         $ref: '#/responses/BadRequest'
+ *       '401':
+ *         $ref: '#/responses/Unauthorized'
+ *       '404':
+ *         $ref: '#/responses/NotFound'
+ */
+
 router.route('/')
   .post(validate(paramValidation.event), eventCtrl.validateEventType, eventCtrl.newEvent);
 
